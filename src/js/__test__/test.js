af59bb89f5127224614d858e5a28283ad6d509a2
YlyaMart <yulya.martinova94@gmail.com>
2023-11-19 14:20:16 +0300

    test.js updated

--- src/js/__test__/test.js
+++ src/js/__test__/test.js
@@ -2,9 +2,9 @@ import sorting from '../app';
 
 test('test', () => {
   const players = [
-    {name: 'мечник', health: 10}, {name: 'маг', health: 100}, {name: 'лучник', health: 80},
+    { name: 'мечник', health: 10 }, { name: 'маг', health: 100 }, { name: 'лучник', health: 80 },
   ];
   const result = sorting(players);
-  const expected = [{name: 'маг', health: 100}, {name: 'лучник', health: 80}, {name: 'мечник', health: 10}];
+  const expected = [{ name: 'маг', health: 100 }, { name: 'лучник', health: 80 }, { name: 'мечник', health: 10 }];
   expect(result).toEqual(expected);
 });
